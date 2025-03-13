1e45d55db4153426af864fa2e465d72cba0e951b
Franco Franks <francotf514@gmail.com>
2025-03-13 15:14:21 -0500

    Include sound class

---
+++ Gamelib/Sound/Sound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Gamelib.Sound
+{
+    public class SoundEffect
+    {
+        public SoundEffectInstance Instance;
+    }
+}
+ 
