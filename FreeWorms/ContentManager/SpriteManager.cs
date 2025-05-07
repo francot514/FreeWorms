acc6a10239003d0a001c88e42efe125d7dc48b65
Franco Franks <francotf514@gmail.com>
2025-05-06 19:32:49 -0500

    Particles included

--- FreeWorms/ContentManager/SpriteManager.cs
+++ FreeWorms/ContentManager/SpriteManager.cs
@@ -1,4 +1,6 @@
-﻿using Gamelib.Image;
+﻿
+using Gamelib.Image;
+using FreeWorms.Animation;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,13 +15,14 @@ namespace FreeWorms.ContentManager
         
         public List<Sprite> Sprites;
         public List<Frame> Frames;
+        public List<Particle> Particles;
 
-        public SpriteManager()
+      public SpriteManager()
         {
 
             Sprites = new List<Sprite>();
             Frames = new List<Frame>();
-
+            Particles = new List<Particle>();
         }
 
 
