1cc7c778b351e9ec9abd2e792abb2742eb95d06a
Franco Franks <francotf514@gmail.com>
2025-09-20 19:07:33 -0500

    Update BinaryReader

--- Gamelib/Common/BinaryReaderEx.cs
+++ Gamelib/Common/BinaryReaderEx.cs
@@ -47,7 +47,8 @@ namespace Gamelib.Common
             }
             if (format == BinaryStringFormat.NoPrefixOrTermination)
             {
-                throw new ArgumentException("NoPrefixOrTermination cannot be used for read operations. Specify the length of the string instead to read strings with no prefix or terminator.");
+                throw new ArgumentException("NoPrefixOrTermination cannot be used for read operations. 
+                Specify the length of the string instead to read strings with no prefix or terminator.");
             }
             throw new ArgumentOutOfRangeException("The specified binary string format is invalid.");
         }
