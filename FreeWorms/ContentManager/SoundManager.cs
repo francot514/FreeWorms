25b9dc4e6f43a6478a9f5cd7e3bf77026bea1606
Franco Franks <francotf514@gmail.com>
2025-10-24 20:22:37 -0500

    Update sound manager

---
+++ FreeWorms/ContentManager/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using Gamelib.Image;
+using Gamelib.Sound;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeWorms.ContentManager
+{
+    public class SoundManager
+    {
+        public Stream CurrentFile;
+        public List<SoundEffect> Sounds;
+        public string SoundsFolder;
+
+        public SoundManager()
+        {
+
+            Sounds = new List<SoundEffect>();
+            SoundsFolder = "DATA/Sound";
+
+        }
+
+    }
+}
