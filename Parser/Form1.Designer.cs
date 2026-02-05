7d64cdb350ca0e23a1607093c219fe8e4ee3b227
Franco Franks <francotf514@gmail.com>
2026-02-04 20:23:56 -0500

    Change title

--- Parser/Form1.Designer.cs
+++ Parser/Form1.Designer.cs
@@ -227,7 +227,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Editor";
+            this.Text = "Game Editor";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
